6140efc46641347bc7704e27b2dbaaba4245fb98
Frank van Leeuwen <frank.vanleeuwen@wolfpack-dcs.com>
2024-12-23 16:24:08 +0100

    Add project files.

---
+++ Dayt.EF.Data/Entities/EventEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Dayt.EF.Entities
+{
+    public class EventEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
---
+++ Dayt.EF.Models/EventModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Dayt.EF.Data
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
